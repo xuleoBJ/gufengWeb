c34369b53dcbd730a5cdedc0b1343c97837ddf5c
xuleoBJ <leoxu_1982@163.com>
2018-03-30 11:25:21 +0800

    20180330am12

---
+++ 1024getNews/123/shushuwu.py
@@ -0,0 +1,82 @@
+##http://941novel.com/archives/795
+
+import requests
+import sys
+import os
+from bs4 import BeautifulSoup
+import random
+import time
+
+type = sys.getfilesystemencoding()
+print(type)
+user_agent_list = [\
+        "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.1 "
+        "(KHTML, like Gecko) Chrome/22.0.1207.1 Safari/537.1",
+        "Mozilla/5.0 (X11; CrOS i686 2268.111.0) AppleWebKit/536.11 "
+        "(KHTML, like Gecko) Chrome/20.0.1132.57 Safari/536.11",
+        "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.6 "
+        "(KHTML, like Gecko) Chrome/20.0.1092.0 Safari/536.6",
+        "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.6 "
+        "(KHTML, like Gecko) Chrome/20.0.1090.0 Safari/536.6",
+        "Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.1 "
+        "(KHTML, like Gecko) Chrome/19.77.34.5 Safari/537.1",
+        "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/536.5 "
+        "(KHTML, like Gecko) Chrome/19.0.1084.9 Safari/536.5",
+        "Mozilla/5.0 (Windows NT 6.0) AppleWebKit/536.5 "
+        "(KHTML, like Gecko) Chrome/19.0.1084.36 Safari/536.5",
+        "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.3 "
+        "(KHTML, like Gecko) Chrome/19.0.1063.0 Safari/536.3",
+        "Mozilla/5.0 (Windows NT 5.1) AppleWebKit/536.3 "
+        "(KHTML, like Gecko) Chrome/19.0.1063.0 Safari/536.3",
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_0) AppleWebKit/536.3 "
+        "(KHTML, like Gecko) Chrome/19.0.1063.0 Safari/536.3",
+        "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.3 "
+        "(KHTML, like Gecko) Chrome/19.0.1062.0 Safari/536.3",
+        "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.3 "
+        "(KHTML, like Gecko) Chrome/19.0.1062.0 Safari/536.3",
+        "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.3 "
+        "(KHTML, like Gecko) Chrome/19.0.1061.1 Safari/536.3",
+        "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.3 "
+        "(KHTML, like Gecko) Chrome/19.0.1061.1 Safari/536.3",
+        "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/536.3 "
+        "(KHTML, like Gecko) Chrome/19.0.1061.1 Safari/536.3",
+        "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.3 "
+        "(KHTML, like Gecko) Chrome/19.0.1061.0 Safari/536.3",
+        "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/535.24 "
+        "(KHTML, like Gecko) Chrome/19.0.1055.1 Safari/535.24",
+        "Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/535.24 "
+        "(KHTML, like Gecko) Chrome/19.0.1055.1 Safari/535.24"
+       ]
+headers={
+        'User-Agent':random.choice(user_agent_list)
+    }
+
+proxyDic = dict(http='http://10.122.14.79:8787',https='http://10.122.14.79:8787')  ##办公室杜lantern
+
+#proxyDic = dict(http='sock5://127.0.0.1:1080',https='sock5://127.0.0.1:1080')  ##办公室杜lantern
+
+filePath = os.path.abspath(__file__)
+osPath = os.path.split(filePath)[0]
+print(filePath)
+
+
+for i in range(38408,38412):
+	pageStrUrl = "http://www.shushuwu.cc/novel/4787/{}.html".format(str(i))
+	print(pageStrUrl)
+	proxy = 0
+	if proxy == 1:
+		page  = requests.get(pageStrUrl,headers = headers,proxies = proxyDic)
+	else:
+		page  = requests.get(pageStrUrl,headers = headers)
+	
+	soup = BeautifulSoup(page.content,'html.parser') # 按照html格式解析页面
+	h1= soup.find("h2")
+	titleStr = h1.contents[0]
+	fileWrited=open(titleStr+".txt",'w',encoding="utf8")
+	inforGet = soup.find_all("div", class_= "yd_text2")
+#	inforGet = soup.find_all("h4")
+	print (inforGet)
+	fileWrited.write(str(inforGet[0]))
+	fileWrited.close()
+
+
