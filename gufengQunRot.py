744540f611377097ef807f76d54148377f347aa3
xuleoBJ <leoxu_1982@163.com>
2018-10-11 17:20:29 +0800

    20181011

--- gufengQunRot.py
+++ gufengQunRot.py
@@ -1,6 +1,7 @@
 import time
 import re
 import os
+import random
 
 def outPinglun(strSpaName,bot, contact):
 	filePath =  os.path.join("D:\qqRotQA\pinglun",strSpaName+".txt")
@@ -10,10 +11,26 @@ def outPinglun(strSpaName,bot, contact):
 			bot.SendTo(contact, sLine)
 	return True
 
+def outKeyWord(strKeyword,bot, contact):
+	filePath =  os.path.join("D:\qqRotQA","关键词.txt")
+	listStrSpa =[]
+	fileOpened=open(filePath,'r')
+	for sLine in fileOpened.readlines():
+		if sLine.strip() !="":
+			splitWord = sLine.split()
+			strSpaName = splitWord[0]
+			if strKeyword in sLine:
+				listStrSpa.append(strSpaName)
+	if len(listStrSpa)>=1:
+		bot.SendTo(contact, ','.join(listStrSpa))
+	else:
+		bot.SendTo(contact, '抱歉，未找到合适信息。')
+	return True
+
 def onQQMessage(bot, contact, member, content):
 	##请问 格式 请问+空格+内容
 	strContent = content
-	if content.strip()!="" and strContent.startswith('请问') :
+	if strContent.strip()!="" and strContent.startswith('请问') :
 		answer  = False
 		if	len(content.strip().split())>=2:
 			strContent = content.split()[1].strip()
@@ -96,46 +113,102 @@ def onQQMessage(bot, contact, member, content):
 				answer  = True
 			if strContent in {'友商谁家按摩最好','谁家按摩好'}:
 				bot.SendTo(contact, '宜生堂')
+				answer  = True		
+		if answer == False :
+			bot.SendTo(contact, '抱歉，您的问题和答案暂时没有收录。')
+	
+	if strContent.strip()!="" and strContent.startswith('有吗') :
+		answer  = False
+		if	len(content.strip().split())>=2:
+			strKeyWord = content.split()[1].strip()
+			outKeyWord(strKeyWord,bot, contact)
+
+	##各店位置
+	if strContent.strip()!="" and strContent.startswith('在哪里') :
+		answer  = False
+		if	len(content.strip().split())>=2:
+			strSpaName = content.split()[1].strip()
+			if strSpaName in '君悦':
+				bot.SendTo(contact, '通州双桥 ')
 				answer  = True
-				
-			##各店点评
-			if strContent in '谧享的特色是啥？':
-				bot.SendTo(contact, '水床AV')
+			if strSpaName in '谧享？':
+				bot.SendTo(contact, '望京东湖渠')
 				answer  = True
-			if strContent in '君悦的特色是啥？':
+			if strSpaName in '百媚':
+				bot.SendTo(contact, '北苑 ')
+				answer  = True
+			if strSpaName in '宜生堂':
+				bot.SendTo(contact, '太阳宫 ')
+				answer  = True
+			if strSpaName in '花涧溪':
+				bot.SendTo(contact, '大屯路')
+				answer  = True
+			if strSpaName in '90后主题':
+				bot.SendTo(contact, '西城马连道 ')
+				answer  = True
+			if strSpaName in '御殿':
+				bot.SendTo(contact, '大屯路 ')
+				answer  = True
+			if strSpaName in '玉颜堂':
+				bot.SendTo(contact, '望京南地铁A口 ')
+				answer  = True
+			if strSpaName in '御盛阁':
+				bot.SendTo(contact, '西城月坛 ')
+				answer  = True
+			if strSpaName in '御仙阁':
+				bot.SendTo(contact, '紫竹桥 ')
+				answer  = True
+			if strSpaName in '樱沐子':
+				bot.SendTo(contact, '魏公村 ')
+				answer  = True
+			if strSpaName in '蜜桃会':
+				bot.SendTo(contact, '双井 ')
+				answer  = True
+			if strSpaName in '舞丝阁':
+				bot.SendTo(contact, '五道口')
+				answer  = True
+			if strSpaName in '青丘':
+				bot.SendTo(contact, '望京南')
+				answer  = True
+		if answer == False :
+			bot.SendTo(contact, '抱歉，您的问题和答案暂时没有收录。')
+			
+	##各店点评
+	if strContent.strip()!="" and strContent.startswith('特色') :
+		answer  = False
+		if	len(strContent.strip().split())>=2:
+			strSpaName = strContent.split()[1].strip()
+			if strSpaName in '君悦':
 				bot.SendTo(contact, '1 极致小尺享受 2 花式诱惑欲罢不能 3 手冰火特色十足 ')
 				answer  = True
-			if strContent in '百媚的特色是啥？':
+			if strSpaName in '谧享？':
+				bot.SendTo(contact, '水床AV')
+				answer  = True
+			if strSpaName in '百媚':
 				bot.SendTo(contact, '1 角色扮演 2 定制制服 3 诱惑舞蹈 4 安全性好，可开发票。 ')
 				answer  = True
-			if strContent in '宜生堂的特色是啥？':
+			if strSpaName in '宜生堂':
 				bot.SendTo(contact, '男士养生好店，环境卫生极好，客服非常用心，三起三落的高端养生项目值得体验，好评率100%。 ')
 				answer  = True
-			if strContent in '花涧溪的特色是啥？':
+			if strSpaName in '花涧溪':
 				bot.SendTo(contact, '态度极好，妩媚妖娆 ')
 				answer  = True
-			if strContent in '90后主题的特色是啥？':
+			if strSpaName in '90后主题':
 				bot.SendTo(contact, '1 安全性非常好 2 技师主打90后 3 态度好，服务标准化，浴室的服务有特色。 ')
 				answer  = True
-			if strContent in '御殿的特色是啥？':
+			if strSpaName in '御殿？':
 				bot.SendTo(contact, '环境非常好，非常好 ')
 				answer  = True
-			if strContent in '玉颜堂的特色是啥？':
+			if strSpaName in '玉颜堂？':
 				bot.SendTo(contact, '有美人兮， 见之不忘。一日不见兮，思之如狂。 ')
 				answer  = True
-			if strContent in '御盛阁的特色是啥？':
+			if strSpaName in '御盛阁？':
 				bot.SendTo(contact, '思悠悠，恨悠悠， 恨到归时方始休，月明人倚楼。 ')
-				answer  = True
-			if strContent in '蜜桃会的哪个美女做饭好吃':
-				bot.SendTo(contact, '客服爱做饭，做饭也好吃')
-			if strContent in {'搞活动的是哪几家?','搞活动的是哪家?'}:
-				bot.SendTo(contact, '玉颜堂')
-				bot.SendTo(contact, '君悦')
 				answer  = True
 		if answer == False :
 			bot.SendTo(contact, '抱歉，您的问题和答案暂时没有收录。')
 		
-	if content.strip()!="" and strContent.startswith('评论') :
+	if strContent.strip()!="" and strContent.startswith('评论') :
 		answer  = False
 		if	len(content.strip().split())>=2:
 			strContent = content.split()[1].strip()		
@@ -200,10 +273,28 @@ def onQQMessage(bot, contact, member, content):
 	##其它有趣			
 	if strContent!="" :
 		if strContent in {'帮助','help','问答'}:
-			bot.SendTo(contact, '句型1 请问+空格+区域（地铁）有吗，例如 1 请问 望京有吗？ 2 请问 10号线有吗？')
-			bot.SendTo(contact, '句型2 请问+空格+店名的特色，例如 1 请问 君悦的特色 2 宜生堂的特色')
-			bot.SendTo(contact, '句型3 请问+空格+店名在哪里，例如 1 请问 青丘在哪里 2 舞丝阁在哪里')
+			bot.SendTo(contact, '句型1 有吗+空格+关键词，例如 1 有吗 望京 2 有吗 10号线')
+			bot.SendTo(contact, '句型2 特色+空格+店名，例如 1 特色 君悦 2 特色 宜生堂')
+			bot.SendTo(contact, '句型3 在哪里+空格+店名，例如 1 在哪里 青丘 2 在哪里 舞丝阁')
 			bot.SendTo(contact, '句型4 评论+空格+店名，例如 1 评论 青丘 2 评论 百媚')
+		if strContent in '讲个笑话':
+			filePath = os.path.join("D:\\qqRotQA\\xiaohua", "笑话.txt")
+			fileOpened=open(filePath,'r')
+			lineList = []
+			for sLine in fileOpened.readlines():
+				if sLine.strip() !="":
+					lineList.append(sLine)
+			iNum = random.randint(0,len(lineList))
+			bot.SendTo(contact, lineList[iNum])
+		if strContent in '讲个成人笑话':
+			filePath = os.path.join("D:\\qqRotQA\\xiaohua", "成人笑话.txt")
+			fileOpened=open(filePath,'r')
+			lineList = []
+			for sLine in fileOpened.readlines():
+				if sLine.strip() !="":
+					lineList.append(sLine)
+			iNum = random.randint(0,len(lineList))
+			bot.SendTo(contact, lineList[iNum])
 		if strContent in '雪哥最爱':
 			bot.SendTo(contact, '君悦冉冉')
 		if strContent == '君悦的婷婷怎么样？':
@@ -218,5 +309,11 @@ def onQQMessage(bot, contact, member, content):
 			bot.SendTo(contact, "当前时间："+localtime)
 		if strContent in '群的网址是什么':
 			bot.SendTo(contact, 'www.gufengBJ.com')
+		if strContent in {'搞活动的是哪几家?','搞活动的是哪家?'}:
+			bot.SendTo(contact, '玉颜堂')
+			bot.SendTo(contact, '君悦')
+		if strContent in '蜜桃会的哪个美女做饭好吃':
+			bot.SendTo(contact, '客服爱做饭，做饭也好吃')
+
 
 	
