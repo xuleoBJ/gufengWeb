9df4dd662a7969b47a598fb7ac2ebbfb1a251596
xuleo <leoxu_1982@163.com>
2018-10-14 11:18:37 +0800

    20181014am11

--- 1024getNews/getLiTitle.py
+++ 1024getNews/getLiTitle.py
@@ -5,7 +5,7 @@ import shutil
 
   
 if __name__=='__main__':
-    sourceDirPath="20180327"
+    sourceDirPath="201810"
     fileNames=os.listdir(sourceDirPath)
     goalFilepath = sourceDirPath+"_"+"LiTitle.txt"
     file_object = open(goalFilepath,'w',encoding='utf8')
