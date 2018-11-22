25db68b4fd484f861973e064ce1591e1f5294784
xuleoBJ <leoxu_1982@163.com>
2018-11-22 17:47:14 +0800

    20181122

--- gufengQunRot.py
+++ gufengQunRot.py
@@ -7,7 +7,7 @@ dataDir = 'E:\\我的坚果云\\qqRotQA'
 
 def outPinglun(strSpaName,bot, contact):
 	if str(contact.name) in ['A小分队','B小分队','C小分队','1K乐优美评论区']:
-		bot.SendTo(contact, '抱歉，本群暂时不公开评论，请加一风好友私聊评论。')
+		bot.SendTo(contact, '抱歉，本群暂时不公开评论，请加一风好友私聊评论。注，临时会话一风不响应。')
 		return False
 	
 	for root, dirs, files in os.walk(os.path.join(dataDir,"pinglun")): 
@@ -36,7 +36,7 @@ def outKeyWord(strKeyword,bot, contact):
 	if len(listStrSpa)>=1:
 		bot.SendTo(contact, '  '.join(listStrSpa))
 	else:
-		bot.SendTo(contact, '抱歉，您的问题和答案暂时没有收录，我们将及时收录。可以访问我们的网站 http://www.gufengBJ.com')
+		bot.SendTo(contact, '抱歉，您的问题和答案我们将及时收录。请输入 问答 查看提问句型或者可以访问我们的网站 http://www.gufengBJ.com')
 	return True
 
 def whereisSpaName(strSpaName,bot, contact):
@@ -75,7 +75,7 @@ def onQQMessage(bot, contact, member, content):
 			bot.SendTo(contact, '--输入 问答 或者 帮助 查看句型引导')
 			bot.SendTo(contact, '--句型1 有吗+空格+关键词，请问+空格+关键词 例如 1 有吗 望京 2 请问 5号线 3 请问 新友商 4 请问 服务好')
 			bot.SendTo(contact, '--句型2 特色+空格+店名，点评+空格+店名 例如 1 特色 君悦 2 点评 宜生堂')
-			bot.SendTo(contact, '--句型3 在哪里+空格+店名，附近+空格+位置 例如 1 在哪 青丘 2 在哪 舞丝阁 3 例如 3 亚运村 附近' )
+			bot.SendTo(contact, '--句型3 在哪里+空格+店名，附近+空格+位置 例如 1 在哪 青丘 2 在哪 舞丝阁 3  亚运村 附近' )
 			bot.SendTo(contact, '--句型4 评论+空格+店名，例如 1 评论 玉颜堂 2 评论 樱沐子')
 			bot.SendTo(contact, '--句型5 输入 友商 查询圈内全部友商，输入 请问 新友商 ， 查看新友商')
 			return  
@@ -129,40 +129,38 @@ def onQQMessage(bot, contact, member, content):
 			
 	##查号	查号+空格+内容
 	if strContent.startswith('查号')  :
-		linesplit=strContent.split()
-		if len(linesplit)==2:
-			qqName = linesplit[0]
-			qqNum = linesplit[1]
-			
-			listLine=[]
-			searchYes = False
+		qqName = "查号"
+		qqNum = strContent.lstrip("查号").strip()
+		
+		listLine=[]
+		searchYes = False
 
-			##搜索黑名单
-			if searchYes == False:
-				filePath = os.path.join(dataDir,"chahao","blackName.txt")
-				fileOpened=open(filePath,'r')
-				for sLine in fileOpened.readlines():
-					splitLine=sLine.split()
-					if qqNum in  sLine:
-						bot.SendTo(contact,'查询结果：'+sLine)
-						searchYes = True
-						break
-			##搜索小分队			
-			if searchYes == False:
-				filePath = os.path.join(dataDir,"chahao","qqInfor.txt")
-				fileOpened=open(filePath,'r')
-				for sLine in fileOpened.readlines():
-					splitLine=sLine.split()
-					if len(splitLine) >= 2 and splitLine[1]==qqNum:
-						qqName =  splitLine[0]
-						searchYes = True
-						break
-				if searchYes == True:
-					bot.SendTo(contact,'查询结果：'+ qqNum +' 是小分队成员，网名: '+ qqName)
-				
+		##搜索黑名单
+		if searchYes == False:
+			filePath = os.path.join(dataDir,"chahao","blackName.txt")
+			fileOpened=open(filePath,'r')
+			for sLine in fileOpened.readlines():
+				splitLine=sLine.split()
+				if qqNum in  sLine:
+					bot.SendTo(contact,'查询结果：'+sLine)
+					searchYes = True
+					break
+		##搜索小分队			
+		if searchYes == False:
+			filePath = os.path.join(dataDir,"chahao","qqInfor.txt")
+			fileOpened=open(filePath,'r')
+			for sLine in fileOpened.readlines():
+				splitLine=sLine.split()
+				if len(splitLine) >= 2 and splitLine[1]==qqNum:
+					qqName =  splitLine[0]
+					searchYes = True
+					break
+			if searchYes == True:
+				bot.SendTo(contact,'查询结果：'+ qqNum +' 是小分队成员，网名: '+ qqName)
 			
-			if searchYes == False:
-				bot.SendTo(contact,'查询结果：'+strContent+  ' 没有查到相关信息。')
+		
+		if searchYes == False:
+			bot.SendTo(contact,'查询结果：'+strContent+  ' 没有查到相关信息。')
 	##其它有趣			
 	if strContent!="" :
 		if strContent in '讲个笑话':
@@ -204,7 +202,7 @@ def onQQMessage(bot, contact, member, content):
 			localtime = time.strftime("%Y-%m-%d-%H:%M")
 			bot.SendTo(contact, "当前时间："+localtime)
 		if strContent in '群的网址' or strContent in '群的网站':
-			bot.SendTo(contact, 'htt://www.gufengBJ.com')
+			bot.SendTo(contact, 'http://www.gufengBJ.com')
 		if strContent in {'搞活动的是哪几家?','搞活动的是哪家?'}:
 			bot.SendTo(contact, '玉颜堂')
 			bot.SendTo(contact, '君悦')
